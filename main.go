cde9f53af853d74ce1920e6c6ab76a32b03ef8bc
Arut Thanomwatana <eaglization@gmail.com>
2018-07-16 18:59:26 +0700

    Init Go and open new worldddddd

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/devilmustcry/gosuki/subject"
+)
+
+func main() {
+	fmt.Print(subject.GetSubject())
+}
---
+++ subject/subject.go
@@ -0,0 +1,14 @@
+package subject
+
+type Subject interface {
+	GetSubject() string
+}
+
+// GetSubject func
+func GetSubject() string {
+	return "Subject"
+}
+
+func getSubject() string {
+	return "Subject"
+}
